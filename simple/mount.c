10f91d168386186f23486cee5c8606de8e123bd2
Ladislav Klenovic <klenovic@nucleonsoft.com>
2011-04-09 13:33:12 +0200

    Add support for ext2

--- simple/mount.c
+++ simple/mount.c
@@ -13,14 +13,13 @@
 #include <minix/config.h>
 #include <minix/const.h>
 #include <minix/minlib.h>
-#include <minix/swap.h>
 #include <sys/svrctl.h>
 #endif
 #include <stdio.h>
 #ifndef __nucleos__
-#include "../../servers/mfs/const.h"
-#else
-#include <servers/mfs/const.h>
+#include "mfs/const.h"
+
+#define MINIX_FS_TYPE "mfs"
 #endif
 
 #define MINIX_FS_TYPE "minixfs"
@@ -28,24 +27,18 @@
 _PROTOTYPE(int main, (int argc, char **argv));
 _PROTOTYPE(void list, (void));
 _PROTOTYPE(void usage, (void));
-_PROTOTYPE(void tell, (char *this));
-_PROTOTYPE(void swapon, (char *file));
-
-#ifndef __nucleos__
-static u8_t MAGIC[] = { SWAP_MAGIC0, SWAP_MAGIC1, SWAP_MAGIC2, SWAP_MAGIC3 };
-#endif
 
 int main(argc, argv)
 int argc;
 char *argv[];
 {
-  int i, swap, n, v, mountflags;
-  char **ap, *vs, *opt, *err, *type, *args;
+  int i, n, v = 0, mountflags, write_mtab;
+  char **ap, *vs, *opt, *err, *type, *args, *device;
   char special[PATH_MAX+1], mounted_on[PATH_MAX+1], version[10], rw_flag[10];
 
   if (argc == 1) list();	/* just list /etc/mtab */
   mountflags = 0;
-  swap = 0;
+  write_mtab = 1;
   type = NULL;
   args = NULL;
   ap = argv+1;
@@ -54,11 +47,11 @@ char *argv[];
 		opt = argv[i]+1;
 		while (*opt != 0) switch (*opt++) {
 		case 'r':	mountflags |= MS_RDONLY;	break;
-		case 's':	swap = 1;			break;
 		case 't':	if (++i == argc) usage();
 				type = argv[i];
 				break;
-		case 'i':	mountflags |= MS_REUSE;		break;                
+		case 'i':	mountflags |= MS_REUSE;		break;
+		case 'n':	write_mtab = 0;			break;
 		case 'o':	if (++i == argc) usage();
 				args = argv[i];
 				break;
@@ -71,41 +64,37 @@ char *argv[];
   *ap = NULL;
   argc = (ap - argv);
 
-  if (mountflags & MS_RDONLY && swap) usage();
+  if (argc != 3 || *argv[1] == 0) usage();
 
-  if (swap) {
-	if (argc != 2) usage();
-#ifndef __nucleos__
-	swapon(argv[1]);
-#endif
-	tell(argv[1]);
-	tell(" is swapspace\n");
-  } else {
-	if (argc != 3) usage();
-	if (mount(argv[1], argv[2], mountflags, type, args) < 0) {
-		err = strerror(errno);
-		std_err("mount: Can't mount ");
-		std_err(argv[1]);
-		std_err(" on ");
-		std_err(argv[2]);
-		std_err(": ");
-		std_err(err);
-		std_err("\n");
-		exit(1);
+  device = argv[1];
+  if (!strcmp(device, "none")) device = NULL;
+
+  if ((type == NULL || !strcmp(type, MINIX_FS_TYPE)) && device != NULL) {
+	/* auto-detect type and/or version */
+	v = fsversion(device, "mount");
+	switch (v) {
+		case FSVERSION_MFS1:
+		case FSVERSION_MFS2:
+		case FSVERSION_MFS3: type = MINIX_FS_TYPE; break;
+		case FSVERSION_EXT2: type = "ext2"; break;
 	}
-	/* The mount has completed successfully. Tell the user. */
-	tell(argv[1]);
-	tell(" is read-");
-	tell(mountflags & MS_RDONLY ? "only" : "write");
-	tell(" mounted on ");
-	tell(argv[2]);
-	tell("\n");
   }
 
+  if (mount(device, argv[2], mountflags, type, args) < 0) {
+	err = strerror(errno);
+	fprintf(stderr, "mount: Can't mount %s on %s: %s\n",
+		argv[1], argv[2], err);
+	exit(1);
+  }
+
+  /* The mount has completed successfully. Tell the user. */
+  printf("%s is read-%s mounted on %s\n",
+	argv[1], mountflags & MS_RDONLY ? "only" : "write", argv[2]);
+
   /* Update /etc/mtab. */
+  if (!write_mtab) return 0;
   n = load_mtab("mount");
   if (n < 0) exit(1);		/* something is wrong. */
-
   /* Loop on all the /etc/mtab entries, copying each one to the output buf. */
   while (1) {
 	n = get_mtab_entry(special, mounted_on, version, rw_flag);
@@ -116,35 +105,27 @@ char *argv[];
 		exit(1);
 	}
   }
-  if (swap) {
-	vs = "swap";
-  } else {
-  	/* For MFS, use a version number. Otherwise, use the FS type name. */
-	if (type == NULL || !strcmp(type, MINIX_FS_TYPE)) {
-		v = fsversion(argv[1], "mount");
-		if (v == 1)
-			vs = "1";
-		else if (v == 2)
-			vs = "2";
-		else if (v == 3)
-			vs = "3";
-		else
-			vs = "0";
-	} else {
-		/* Keep the version field sufficiently short. */
-		if (strlen(type) < sizeof(version))
-			vs = type;
-		else
-			vs = "-";
+  /* For MFS, use a version number. Otherwise, use the FS type name. */
+  if (!strcmp(type, MINIX_FS_TYPE)) {
+	switch (v) {
+		case FSVERSION_MFS1: vs = "1"; break;
+		case FSVERSION_MFS2: vs = "2"; break;
+		case FSVERSION_MFS3: vs = "3"; break;		
+		default: vs = "0"; break;
 	}
+  } else {
+	/* Keep the version field sufficiently short. */
+	if (strlen(type) < sizeof(version))
+		vs = type;
+	else
+		vs = "-";
   }
-  n = put_mtab_entry(argv[1], swap ? "swap" : argv[2], vs,
+  n = put_mtab_entry(argv[1], argv[2], vs,
 		     (mountflags & MS_RDONLY ? "ro" : "rw") );
   if (n < 0) {
 	std_err("mount: /etc/mtab has grown too large\n");
 	exit(1);
   }
-
   n = rewrite_mtab("mount");
   return(0);
 }
@@ -162,16 +143,9 @@ void list()
   while (1) {
 	n = get_mtab_entry(special, mounted_on, version, rw_flag);
 	if  (n < 0) break;
-	write(1, special, strlen(special));
-	if (strcmp(version, "swap") == 0) {
-		tell(" is swapspace\n");
-	} else {
-		tell(" is read-");
-		tell(strcmp(rw_flag, "rw") == 0 ? "write" : "only");
-		tell(" mounted on ");
-		tell(mounted_on);
-		tell("\n");
-	}
+	printf("%s is read-%s mounted on %s (type %s)\n",
+		special, strcmp(rw_flag, "rw") == 0 ? "write" : "only",
+		mounted_on, version);
   }
   exit(0);
 }
@@ -179,63 +153,6 @@ void list()
 
 void usage()
 {
-  std_err("Usage: mount [-r] [-t type] [-o options] special name\n"
-  	  "       mount -s special\n");
+  std_err("Usage: mount [-r] [-e] [-t type] [-o options] special name\n");
   exit(1);
 }
-
-
-void tell(this)
-char *this;
-{
-  write(1, this, strlen(this));
-}
-
-#ifndef __nucleos__
-void swapon(file)
-char *file;
-{
-  u32_t super[2][_MAX_BLOCK_SIZE / 2 / sizeof(u32_t)];
-  swap_hdr_t *sp;
-  struct mmswapon mmswapon;
-  int fd, r;
-  char *err;
-  
-  if ((fd = open(file, O_RDWR)) < 0
-	|| lseek(fd, SUPER_BLOCK_BYTES, SEEK_SET) == -1
-	|| (r = read(fd, super, _STATIC_BLOCK_SIZE)) < 0
-  ) {
-	err = strerror(errno);
-	std_err("mount: ");
-	std_err(file);
-	std_err(": ");
-	std_err(err);
-	std_err("\n");
-	exit(1);
-  }
-  sp = (swap_hdr_t *) &super[0];
-  if (memcmp(sp->sh_magic, MAGIC, sizeof(MAGIC)) != 0)
-	sp = (swap_hdr_t *) &super[1];
-  if (r == _STATIC_BLOCK_SIZE && memcmp(sp->sh_magic, MAGIC, sizeof(MAGIC)) != 0
-			|| sp->sh_version > SH_VERSION) {
-	std_err("mount: ");
-	std_err(file);
-	std_err(" is not swapspace\n");
-	exit(1);
-  }
-  close(fd);
-  mmswapon.offset = sp->sh_offset;
-  mmswapon.size = sp->sh_swapsize;
-  strncpy(mmswapon.file, file, sizeof(mmswapon.file));
-  mmswapon.file[sizeof(mmswapon.file)-1] = 0;
-  if (svrctl(MMSWAPON, &mmswapon) < 0) {
-	err = strerror(errno);
-	std_err("mount: ");
-	std_err(file);
-	std_err(": ");
-	std_err(err);
-	std_err("\n");
-	exit(1);
-  }
-}
-#endif /* __nucleos__ */
